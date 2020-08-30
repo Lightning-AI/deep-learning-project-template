216e74b294d96be19077c807ede44fc4035b06b1
Jirka Borovec <jirka.borovec@seznam.cz>
2020-08-30 23:49:45 +0200

    format config

--- setup.py
+++ setup.py
@@ -2,15 +2,14 @@
 
 from setuptools import setup, find_packages
 
-setup(name='research_mnist',
-      version='0.0.0',
-      description='Describe Your Cool Project',
-      author='',
-      author_email='',
-      url='https://github.com/PyTorchLightning/pytorch-lightning-conference-seed',  # REPLACE WITH YOUR OWN GITHUB PROJECT LINK
-      install_requires=[
-            'pytorch-lightning'
-      ],
-      packages=find_packages()
-      )
-
+setup(
+    name='research_mnist',
+    version='0.0.0',
+    description='Describe Your Cool Project',
+    author='',
+    author_email='',
+    # REPLACE WITH YOUR OWN GITHUB PROJECT LINK
+    url='https://github.com/PyTorchLightning/pytorch-lightning-conference-seed',
+    install_requires=['pytorch-lightning'],
+    packages=find_packages(),
+)
