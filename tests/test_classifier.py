70d11575aef4c73f95b542c57a71cffb688aab2c
William Falcon <waf2107@columbia.edu>
2020-09-11 11:04:54 -0400

    clean up sample project

---
+++ tests/test_classifier.py
@@ -0,0 +1,15 @@
+from project.datasets.mnist import mnist
+from project.lit_classifier_main import LitClassifier
+from pytorch_lightning import Trainer, seed_everything
+
+
+def test_lit_classifier():
+    seed_everything(1234)
+
+    model = LitClassifier()
+    train, val, test = mnist()
+    trainer = Trainer(limit_train_batches=50, limit_val_batches=20, max_epochs=2)
+    trainer.fit(model, train, val)
+
+    results = trainer.test(test_dataloaders=test)
+    assert results[0]['test_acc'] > 0.7
