5985a6616ba4cae238728917801c5618bde3a125
knoriy <knoriy@ip-172-31-36-138.ec2.internal>
2022-08-26 12:11:01 +0000

    bug where using multiple nodes would linger when initiating gpus

--- slurm.sh
+++ slurm.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-#SBATCH --partition=compute-od-gpu
-#SBATCH --job-name=pl_test
-#SBATCH --nodes=10
+#SBATCH --partition=gpu
+#SBATCH --job-name=multinode_pl_test
+#SBATCH --nodes=2
 #SBATCH --exclusive
+#SBATCH --comment clap
+#SBATCH --ntasks-per-node=8
 #SBATCH --output=%x_%j.out
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/nccl/build/lib:/opt/aws-ofi-nccl-install/lib
@@ -18,5 +20,6 @@ export FI_EFA_ENABLE_SHM_TRANSFER=0
 export FI_PROVIDER=efa
 export FI_EFA_TX_MIN_CREDITS=64
 export NCCL_TREE_THRESHOLD=0
+export NCCL_SOCKET_IFNAME=^docker0,lo
 
-srun /home/knoriy/fsx/miniconda3/envs/pl/bin/python /home/knoriy/deep-learning-project-template/project/lit_mnist.py --accelerator gpu --strategy ddp
+srun --comment clap /home/knoriy/fsx/miniconda3/envs/clasp/bin/python /home/knoriy/deep-learning-project-template/project/lit_mnist.py --accelerator gpu --strategy ddp --num_nodes 2 --devices 8
